79405f134d9cc84384dd17c78977042ef40658e8
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-06-11 15:34:51 -0400

    se añadió validación de campos obligatorios

--- ejercicio11.js
+++ ejercicio11.js
@@ -1,16 +1,31 @@
 /* 
 dado un texto y una busuqeda censurar  todas las coincidencias de las busqueda en el texto con "[-CENSURADO-]"
-
 si el texto y la busqueda estan vacias mostrar " no puedes leer el texto y la busqueda" , en el caso de que ambos parametros no llegue
 */
 
 
 
     const p = 'The quick brown fox jumps over dog the lazy dog. If the dog reacted, was it really lazy?';
-    const censurado = (cadena,busqueda) => {     
+    const censurado = (cadena,busqueda) => {   
+    let patronBusqueda = new RegExp(busqueda, 'gi'); // creo el patron para realizar la busqueda , esta es la unica manera de poder pasar la variable a la expresión regular.
+    
+    if(cadena && busqueda)
+        resultado = cadena.replace(patronBusqueda, '[-CENSURADO-]');
+    else if(cadena && !busqueda)
+        resultado = `falta el patron de busqueda \n`;
+    else if(!cadena && busqueda)        
+        resultado = `falta la cadena de texto \n`;    
+    else
+        resultado = `no hay datos ingresados \n`;    
 
-        let patronBusqueda = new RegExp(busqueda, 'gi'); // creo el patron para realizar la busqueda , esta es la unica manera de poder pasar la variable a la expresión regular.
-        return cadena.replace(patronBusqueda, '[-CENSURADO-]');            
+    return resultado;    
+        
+
+
+
+
     }
 
     console.log(`${censurado(p,`dog`)}`);
+
+
