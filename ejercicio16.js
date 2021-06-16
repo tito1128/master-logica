fcfea94663edf216c27de6f9fcc210bf4242c91c
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-06-16 12:14:21 -0400

    se modifica función , mostrando la cantidad de vocales que posee la cadena

--- ejercicio16.js
+++ ejercicio16.js
@@ -3,14 +3,21 @@ dado una cadena de texto devolver cuantas vocales tiene :
 ejemplo alejandro : 4 vocales
 */
 
+const cadena = 'lavacalolatienecabezaytienecola';
 const buscaVocales = cadena => {
-    let arrayMapa = {} , vecesRepetida = 0; // creo variables
+    let arrayMapa = {} , vecesRepetida = 0 , totalVocales = 0; // creo variables
     const arrayResultado = cadena.match(/[aeiou]/gi); // genero nuevo array con patron de busqueda ( vocales )
-    for (let vocales of arrayResultado){ // recorro los valores del array generado 
+    arrayResultado ? totalVocales = arrayResultado.length : totalVocales = 0;
+
+    /*for (let vocales of arrayResultado){ // recorro los valores del array generado 
         !arrayMapa[vocales] ? vecesRepetida = 1 : vecesRepetida = arrayMapa[vocales] + 1;  // creo un nuevo array para procesar los valores de repetición  
         arrayMapa[vocales] = vecesRepetida; // agrego valores al array
-    }
-    return arrayMapa;
+    }*/
+    //return arrayMapa;
+
+
+
+    return totalVocales;
 }
 
-console.log(buscaVocales('lavacalolatienecabezaytienecola'));
+console.log(`la cadena "${cadena}"  tiene ${buscaVocales(cadena)} vocales`);
