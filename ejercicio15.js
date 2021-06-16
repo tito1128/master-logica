8f5d9a195460460ea8656165039009731665a3f6
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-06-16 10:39:24 -0400

    se crea un array mostrando la palabra , la letra y las veces que se repite esa letra en la cadena de texto

--- ejercicio15.js
+++ ejercicio15.js
@@ -7,13 +7,32 @@ Resultado : o se repite 2 veces
 
 const devuelveCaracterRepetido = cadena => {
     let descomponeArray = cadena.split(''); 
-    let arrayFinal = {} , contador = 0;  
+    let arrayMapa = {} , vecesRepetida = 0 , letraRepetida = '';  
     for(let a of descomponeArray){        
-        !arrayFinal[a] ? contador = 1 : contador = arrayFinal[a] + 1;        
-         arrayFinal[a] = contador;
-   }
-   return max;
+        !arrayMapa[a] ? vecesRepetida = 1 : vecesRepetida = arrayMapa[a] + 1;       
+        if (arrayMapa[a] >= vecesRepetida) 
+            vecesRepetida = arrayMapa[a];
+            letraRepetida = a;        
+        arrayMapa[a] = vecesRepetida;
+   } 
+   
+   return {palabra:cadena,letra:letraRepetida,"repeticiones":vecesRepetida};
+
+  
+
+ 
+
+   /*const arrKey = Object.keys(arrayMapa).reduce((a, b) => arrayMapa[a] > arrayMapa[b] ? a : b);
+   const arrValue = Object.values(arrayMapa).reduce((a, b) => arrayMapa[a] > arrayMapa[b] ? a : b);
+   console.log(arrKey);
+   console.log(arrValue);*/
+
+   // obtenglo el valor maximo y minimo , pero me falta obtener la id o key del arreglo
+   /*let arr = Object.values(arrayMapa) , idArr = Object.keys(arrayMapa);
+   let min = Math.min(...arr);
+   let max = Math.max(...arr);*/
+   
 }
 
 
-console.log(devuelveCaracterRepetido("holaa"));
+console.log(devuelveCaracterRepetido("lavacalolcatienecabezaytienecola"));
