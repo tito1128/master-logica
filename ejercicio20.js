f732f6554d4c8ed29eac78180b78df8d185655c8
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-06-19 12:24:01 -0400

    ejercicio realiza lo solicitado pero se debe corregir la identación

---
+++ ejercicio20.js
@@ -0,0 +1,32 @@
+/* dadas dos cadenas de texto, crear un algoritmo que compruebe si son anagramas entre si
+
+Una cadena es una anagrama de otra si usa los mismos caracteres en la misma cantidad
+
+Ejemplo 
+            Riesgo - Sergio = devuelve True
+            Victor Robles  devuelve False
+*/
+
+
+const anagrama = (str1,str2) =>{
+    let array1 = '' , array2 = '' , resultado ='';
+    if (str1!=null && str2!=null)
+        array1 = str1.toLowerCase().replace(/\W/gi,'').split('');
+        array2 = str2.toLowerCase().replace(/\W/gi,'').split('');
+        if(str1.length == str2.length)
+            for(letras of array1){
+                if( array2.includes(letras) === false)
+                    return false
+                    resultado =` ${str1} y ${str2} no son anagramas\n`                    
+                else
+                    resultado =`${str1} y ${str2} si son anagramas\n`     
+            }
+        else  resultado = `las cadenas no son anagramas`;
+        
+     
+    return resultado;
+    
+   
+}
+
+console.log(anagrama('lapo','lalo'));
