9cc83466e194ec32c096df6bae2e32d91ab0f7a1
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-07-20 20:34:46 -0400

    dado un array de numeros devolverlo ordenado desde el menor al mayor

--- ejercicio40.js
+++ ejercicio40.js
@@ -1,9 +1,16 @@
 /*
     dado un array de numeros devolverlo ordenado desde el menor al mayor
+    y devolver el primer y ultimo elemento
 */
 
 const arrOrdenado = arr =>{
-    return arr.sort((a,b) =>a-b); // si se invierte B - A queda ordenado de mayor a menor
+    //return arr.sort((a,b) =>a-b); // si se invierte B - A queda ordenado de mayor a menor
+    
+   return `primero: ${arr[0]}
+        ultimo: ${arr[arr.length - 1]}
+        resultado: ${arr.sort((a,b) =>a-b)}`
+    
+
     //return arr.sort((a,b)=>a-b);
 }
 
