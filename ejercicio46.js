5d9100894cb8f408b985e00e8e233cb0fcecbf31
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-07-28 17:19:52 -0400

    cambio en el planteamiento del problema

--- ejercicio46.js
+++ ejercicio46.js
@@ -1,22 +1,34 @@
 /*
-Dado un string devolver todos los posibles sub string
+Dado un string devolver todas las posibles combinaciones con ese string
 */
 
 const randomString = texto =>{
-    let array = [...texto],
-        delElement ='',
-        nuevaPalabra = '';     
-
+    let array = [...texto], delElement ='', nuevaPalabra = '';     
         for(let dato of array){
             delElement = array.shift(); // obtengo el primero elemento a eliminar 
             array.push(delElement); // agrego el elemento a eliminar a la ultima posición            
             nuevaPalabra+= `${array.join('')}\n`;  // concateno todos los array combinaciones posibles y los devuelvo como string                    
         }   
-
         return nuevaPalabra;
 }
+//console.log(`${randomString("holamundo")}`);
 
-console.log(`${randomString("holamundo")}`);
+
+const todoSubstring = string =>{
+    let arrString =[];
+        for(let i = 0; i <= string.length; i++){
+            let inicio = i;
+            for(let n = 0; n <= parseInt(string.length - inicio); n++){
+                let final = parseInt(inicio) + parseInt(n);
+                if(string.substring(inicio,final) != '')
+                    arrString.push(string.substring(inicio,final));                                      
+            }
+        }        
+        return arrString;
+}
+
+
+console.log(todoSubstring("hola"));
 
 
 
