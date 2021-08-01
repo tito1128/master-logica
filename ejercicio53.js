507de8c673545a2a6f1a08be261546065a471435
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-07-31 23:25:42 -0400

    se eliminó array map y se ocupó array filter para optimizar el codigo junto con el resultado

--- ejercicio53.js
+++ ejercicio53.js
@@ -2,15 +2,10 @@
  dado un array de numeros , devolver un array nuevo con los numeros pares e impares
 */
 
-
-
 const recorreArray = arr =>{
-    const nuevoArray = [];
-    arr.map(numero =>{        
-        if(numero % 2 == 0) nuevoArray.push(numero);
-    })
-
-    return nuevoArray;
+    const pares = arr.filter((numero) => numero % 2 == 0);
+    const impares = arr.filter((numero) => numero % 2 != 0);
+    return {"pares":pares,"impares":impares}    
 }
 
 console.log(recorreArray([1,2,4,5,6,6,78,13,352,5,23]));
