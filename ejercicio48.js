ab7dfec32a7c4bc357dd935ded22602175d9be86
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-07-31 22:26:45 -0400

    quité el array filter y lo cambié por un array map en donde hice el filtrado y agregué la key del array la cual corresponde a la letra

--- ejercicio48.js
+++ ejercicio48.js
@@ -3,24 +3,37 @@ y cual es la primera que aparece */
 
 
 const soloUnaVez = string =>{
-let result = {};
+let result = {} , letrasUnicas = [];
+
 [...string].map((valor,key)=>{  
    if(valor !==' ' && valor !== null)
    result[valor] = (result[valor] || 0) + 1;   
 });
 
+Object.keys(result).map(letra => {
+ if(result[letra] === 1) letrasUnicas.push(letra);
+})
 
-// convierto result en una matriz valor y clave , y la enumera como un for IN
+
+ console.log({"Primera Letra":Object.keys(result)[0],"Letras Unicas":letrasUnicas});
+
+
+
+
+
+/*
+
+// convierto result en una matriz valor y clave [clave,valor],[clave,valor], y la enumera como un for IN
 const objeto = Object.entries(result);
 // ya que el objeto es tratado como un array , puedo utilizar filter array , si declaro la clave y el valor
 const unicos = objeto.filter(([key, value]) => value == 1);
 // vuelvo a convertir el array en objeto
-const resultadoUnicos = Object.fromEntries(unicos);
+const letras = Object.fromEntries(unicos);
+console.log({"Primera Letra":Object.keys(result)[0],"letras unicas":letras});
 
-console.log(resultadoUnicos);
-
+*/
 
   
 }
 
-console.log(`${soloUnaVez('hola amigo')}`);
+soloUnaVez('hola amigo');
