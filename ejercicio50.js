f570ba2c90fa5f88f935ee8cccc318a0c58d7bfa
tito1128 <alejandro.a.c.cepeda@gmail.com>
2021-07-31 23:00:35 -0400

    dado un numero, devolver cuantos bucles tiene un numero tiene un bucle cuando tiene un circulo cerrado cuando lo dibujas , lo que 6 tiene un bucle ,1 no tiene bucle y 8 tiene dos bucles

---
+++ ejercicio50.js
@@ -0,0 +1,29 @@
+/*
+dado un numero, devolver cuantos bucles tiene
+un numero tiene un bucle cuando tiene un circulo cerrado
+cuando lo dibujas , lo que 6 tiene un bucle ,1 no tiene bucle y 8 tiene dos bucles
+*/
+
+
+let bucles = {
+    '0':1,
+    '1':0,
+    '2':0,
+    '3':0,
+    '4':0,
+    '5':0,
+    '6':1,
+    '7':0,
+    '8':2,
+    '9':1
+}
+
+const cuantosBucles = numero =>{
+let arrNumero = numero.toString() , resultado = 0;
+[...arrNumero].map((valor,key)=>{      
+   resultado = parseInt(resultado)  + parseInt(bucles[valor]); 
+})
+return {"numero":numero,"bucles":resultado}
+}
+
+console.log(cuantosBucles(1239860));
